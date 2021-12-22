7d3a1f27b9843d612b03d762368248a6388b2439
Forest Anderson <forestkzanderson@gmail.com>
2021-12-21 21:49:32 -0500

    Trying to fix compile

--- tree-writer/src/main.rs
+++ tree-writer/src/main.rs
@@ -2,7 +2,7 @@ use spidev::{SpiModeFlags, Spidev, SpidevOptions, SpidevTransfer};
 use std::{io, thread, time::Duration};
 use tree_writer::FRAME_RATE;
 
-use mun_runtime::{invoke_fn, RuntimeBuilder};
+// use mun_runtime::{invoke_fn, RuntimeBuilder};
 
 mod render;
 use render::build_array;
@@ -28,22 +28,26 @@ fn full_duplex(spi: &mut Spidev, tx_buf: [u8; 4500]) -> io::Result<()> {
     Ok(())
 }
 
-fn main() {
-    let mut runtime = RuntimeBuilder::new("../../tree-script/target/mod.munlib")
-        .spawn()
-        .expect("Failed to spawn Runtime");
+// TODO: Try this when we figure out Mun arrays
+// fn load_from_mun() {
+//     let mut runtime = RuntimeBuilder::new("../../tree-script/target/mod.munlib")
+//         .spawn()
+//         .expect("Failed to spawn Runtime");
 
+//     runtime.borrow_mut().update();
+//     let runtime_ref = runtime.borrow();
+
+//     let tx_buf: i32 = invoke_fn!(runtime_ref, "build_array", tick).unwrap();
+// }
+
+fn main() {
     let mut spi = create_spi().unwrap();
 
     let mut tick = 0;
 
     loop {
         thread::sleep(Duration::from_millis(1000 / FRAME_RATE));
-        runtime.borrow_mut().update();
 
-        let runtime_ref = runtime.borrow();
-        let tx_buf: [u8; 4500] = invoke_fn!(runtime_ref, "build_array", tick).unwrap();
-        
         let tx_buf = build_array(tick);
         full_duplex(&mut spi, tx_buf).unwrap();
         tick += 1;
