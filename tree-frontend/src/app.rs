ab3fd606a4ccdffd0d089cb9c1fad16808ed81c4
Forest Anderson <forestkzanderson@gmail.com>
2021-12-22 15:47:44 -0500

    Frontend work

--- tree-frontend/src/app.rs
+++ tree-frontend/src/app.rs
@@ -1,5 +1,5 @@
 use reqwasm::http::Request;
-use tree_writer::{Renderer, RendererStrings};
+use tree_data_schema::{Renderer, RendererStrings};
 use yew::prelude::*;
 
 #[function_component(App)]
@@ -11,31 +11,45 @@ fn app() -> Html {
             move |_| {
                 let renderers = renderers.clone();
                 wasm_bindgen_futures::spawn_local(async move {
-                    let renderer_strings: Vec<Renderer> =
-                        Request::get("/renderers")
-                            .send()
-                            .await
-                            .unwrap()
-                            .json()
-                            .await
-                            .unwrap();
+                    let renderer_strings: Vec<Renderer> = Request::get("/renderers")
+                        .send()
+                        .await
+                        .unwrap()
+                        .json()
+                        .await
+                        .unwrap();
                     renderers.set(renderer_strings);
                 });
                 || ()
             },
             (),
         );
-
-        let renderers_iter = renderers.as_ref();
     }
+
+    let selected_renderer = use_state(|| None);
+
+
+    let on_video_select = {
+        let selected_renderer = selected_renderer.clone();
+        Callback::from(move |renderer: Renderer| selected_renderer.set(Some(renderer)))
+    };
 
     html! {
         <>
-            <h1>{ "RustConf Explorer" }</h1>
+            <h1>{ "Tree Patterns" }</h1>
             <div>
-                <RendererButton renderer={videos} on_click={on_video_select.clone()} />
+                {
+                    renderers.into_iter().map(|r| {
+                        let renderer = r.clone();
+                        html! {
+                            <>
+                            <RendererButton renderer={renderer} on_click={on_video_select.clone()} />
+                            </>
+                        }
+                    }).collect::<Html>()
+                }
+
             </div>
-            { for renderers_iter }
         </>
     }
 }
@@ -43,20 +57,20 @@ fn app() -> Html {
 #[derive(Properties, PartialEq)]
 struct RendererButtonProps {
     renderer: Renderer,
-    on_click: Callback<Video>,
+    on_click: Callback<Renderer>,
 }
 
 #[function_component(RendererButton)]
-fn renderer_button(props: &RendererButtonProps) -> Html {
-    let on_click = use_state(|| false);
-    let on_click = on_click.clone();
-    let on_click = move |_| {
-        on_click.set(!on_click.get());
+fn renderer_button(RendererButtonProps { renderer, on_click }: &RendererButtonProps) -> Html {
+    let on_renderer_click = {
+        let on_click = on_click.clone();
+        let renderer = renderer.clone();
+        Callback::from(move |_| on_click.emit(renderer.clone()))
     };
 
     html! {
-        <button onclick=on_click>
-            { props.videos.len() }
+        <button onclick={on_renderer_click}>
+            { renderer.name.clone() }
         </button>
     }
 }
--- tree-frontend/src/main.rs
+++ tree-frontend/src/main.rs
@@ -2,8 +2,6 @@ mod app;
 
 use app::App;
 
-mod text_input;
-
 fn main() {
     yew::start_app::<App>();
 }
