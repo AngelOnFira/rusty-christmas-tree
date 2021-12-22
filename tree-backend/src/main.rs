f954d9011466f9f7122ad14d6fa5750acfb51374
Forest Anderson <forestkzanderson@gmail.com>
2021-12-22 14:49:09 -0500

    Basic backend

---
+++ tree-backend/src/main.rs
@@ -0,0 +1,37 @@
+use std::sync::{Arc, Mutex};
+
+use warp::Filter;
+
+use tree_writer::{renderer_strings, Renderer, RendererStrings, Renderers};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let current_renderer = Arc::new(Mutex::new(Renderers::RedLines));
+
+    let renderers = warp::path!("renderers").map(move || {
+        warp::reply::json({
+            let strings = renderer_strings();
+
+            // Find the one that is the current renderer and set to true
+            &RendererStrings {
+                renderers: strings
+                    .iter()
+                    .map(|r| {
+                        let mut renderer = r.clone();
+                        if *current_renderer.lock().unwrap() == renderer.name {
+                            renderer.enabled = true;
+                        }
+                        renderer
+                    })
+                    .collect(),
+            }
+        })
+    });
+
+    let cors = warp::cors().allow_any_origin();
+    let routes = warp::get().and(renderers).with(cors);
+
+    warp::serve(routes).run(([0, 0, 0, 0], 3030)).await;
+
+    Ok(())
+}
