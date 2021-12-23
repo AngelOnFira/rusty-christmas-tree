2e03b4d5927f0f7ac7a2cf3a32ac62f3315f91c9
Elias Hawa <elias.hawa2468@gmail.com>
2021-12-23 10:19:38 -0500

    Create rainbow wave tick pattern

--- tree-writer/src/renderers/mod.rs
+++ tree-writer/src/renderers/mod.rs
@@ -7,6 +7,7 @@ pub mod red_wave;
 pub mod template;
 pub mod snow;
 pub mod ender_logo;
+pub mod rainbow_wave;
 
 pub mod tree_canvas;
 
@@ -17,5 +18,6 @@ pub fn visualize_renderer(tick: u64, renderer: Renderers) -> TreeCanvas {
         Renderers::RedWave => red_wave::draw(tick),
         Renderers::Template => template::draw(tick),
         Renderers::Snow => snow::draw(tick),
+        Renderers::RainbowWave => rainbow_wave::draw(tick),
     }
 }
