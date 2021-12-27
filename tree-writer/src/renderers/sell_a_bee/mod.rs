426be3d669ccf8333dbd114ecab55cde56578746
Liam H <5874952+bigforestnotrees@users.noreply.github.com>
2021-12-27 10:24:47 -0500

    Converted video frames to JSON file. Updated draw to reflect change in format. Added caching on video read.

--- tree-writer/src/renderers/sell_a_bee/mod.rs
+++ tree-writer/src/renderers/sell_a_bee/mod.rs
@@ -1,34 +1,35 @@
 use std::cmp::min;
 
-use image::io::Reader as ImageReader;
-use image::imageops::{self};
-use image::{Rgb};
-
-use std::env;
-
 use super::{Pixel, TreeCanvas};
+
+use serde::Deserialize;
+use serde_json;
+
+use cached::proc_macro::cached;
 
 // Name: Sell a bee
 // Description: Celebi's got some new moves in this render!
 // Author: Liam Henderson
+#[cached(size=1)]
+fn get_video(n: u8) -> Vec<Vec<Vec<Pixel>>> {
+    let video = include_str!("video.json");
+    serde_json::from_str(&video).unwrap()
+}
 
 pub fn draw(tick: u64) -> TreeCanvas {
     let mut canvas = TreeCanvas::new();
 
-    let frame_num = tick % 249 + 1;
-    let frame_name = format!("tree-writer/src/renderers/sell_a_bee/right resolution/{:0>4}.png", frame_num);
-    let frame = ImageReader::open(frame_name).unwrap().decode().unwrap().resize_exact(20, 75, imageops::FilterType::Gaussian);
     
+    let frame_num : usize = (tick % 250).try_into().unwrap();
+
+    let parsed: Vec<Vec<Vec<Pixel>>> = get_video(0);
+
+    let frame: Vec<Vec<Pixel>> = parsed[frame_num].clone();
 
     for y in 0..75 {
         for x in 0..20 {
-            let pixel: Rgb<u8> = *frame.clone().into_rgb8().get_pixel(x, 74-y);
-            let this_pixel = Pixel {
-                r: pixel[0],
-                g: pixel[1],
-                b: pixel[2],
-            };
-            canvas.set_pixel(x.try_into().unwrap(), y.try_into().unwrap(), this_pixel)
+            let pixel: Pixel = frame[y][x];
+            canvas.set_pixel(x, 74-y, pixel)
         }
     }
 
